926e54443299921ae696f572abb83b8693689583
Artem Trofimov <trofimov9artem@gmail.com>
2013-06-04 23:35:31 +0400

    Initial Commit

---
+++ WaterSimulation/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  WaterSimulation
+//
+//  Created by Artem Trofimov on 04.06.13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ WaterSimulation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WaterSimulation
+//
+//  Created by Artem Trofimov on 04.06.13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
