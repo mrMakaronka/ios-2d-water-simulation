c41c3be3dde1cc63c3113b8cab06336cfb1dcf9e
Artem Trofimov <trofimov9artem@gmail.com>
2013-06-04 23:41:30 +0400

    Added node for drawing water

---
+++ WaterSimulation/WaterNode.h
@@ -0,0 +1,35 @@
+#import "cocos2d.h"
+#import "WaterColumn.h"
+
+#define COLUMN_COUNT 60
+#define WATER_HEIGHT 200
+
+#define TENSION 0.025f
+#define DAMPENING 0.025f
+#define SPREAD 0.25f
+
+typedef struct Trapezoid {
+    CGPoint upperLeft;
+    CGPoint upperRight;
+    CGPoint bottomLeft;
+    CGPoint bottomRight;
+} Trapezoid;
+
+typedef struct TrapezoidColor {
+    ccColor4B upperLeft;
+    ccColor4B upperRight;
+    ccColor4B bottomLeft;
+    ccColor4B bottomRight;
+} TrapezoidColor;
+
+@interface WaterNode : CCNode {
+    Trapezoid _trapezoidArray[COLUMN_COUNT];
+    TrapezoidColor _trapezoidColorsArray[COLUMN_COUNT];
+    NSMutableArray *_columns;
+    CGFloat _scale;
+}
+
+-(id)init;
+-(void)Splash:(CGFloat)index :(CGFloat) speed;
+
+@end
