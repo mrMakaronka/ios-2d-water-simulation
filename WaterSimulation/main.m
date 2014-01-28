8ffae36f72e96f410816785f1b41ec99e81358fd
Artem Trofimov <trofimov9artem@gmail.com>
2014-01-29 03:41:53 +0400

    Refactoring (removing bad practices & improving code style)

--- WaterSimulation/main.m
+++ WaterSimulation/main.m
@@ -1,13 +1,3 @@
-//
-//  main.m
-//  WaterSimulation
-//
-//  Created by Artem Trofimov on 04.06.13.
-//  Copyright __MyCompanyName__ 2013. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
